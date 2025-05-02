2e7642a417bf50dfd909e4689445b3c661609e61
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-05-01 23:10:49 -0300

    Adds input component without styling

--- my-app/src/features/posts/components/CreatePostSection/CreatePostSection.tsx
+++ my-app/src/features/posts/components/CreatePostSection/CreatePostSection.tsx
@@ -1,11 +1,33 @@
+import { useState } from "react"
+import Input from "../../../../components/Input/Input"
+import TextArea from "../../../../components/TextArea/TextArea"
 import styles from "./CreatePostSection.module.css"
 
-const createPostSection = () => {
+const CreatePostSection = () => {
+  const [title, setTitle] = useState("")
+  const [content, setContent] = useState("")
+
   return (
     <div className={styles.createPostSection}>
       <h2 className={styles.heading}>What's on your mind?</h2>
+      <Input
+        label="Title"
+        name="title"
+        value={title}
+        onChange={e => {
+          setTitle(e.target.value)
+        }}
+      />
+      <TextArea
+        label="Content"
+        name="content"
+        value={content}
+        onChange={e => {
+          setContent(e.target.value)
+        }}
+      />
     </div>
   )
 }
 
-export default createPostSection
+export default CreatePostSection
