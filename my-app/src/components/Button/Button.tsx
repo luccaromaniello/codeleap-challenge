384a4c5479ee83207d248c061f489d61e928acd0
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-05-02 00:00:07 -0300

    Adds button to create post section

---
+++ my-app/src/components/Button/Button.tsx
@@ -0,0 +1,22 @@
+import styles from "./Button.module.css"
+
+type ButtonProps = {
+  text: string
+  onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void
+  disabled?: boolean
+}
+
+const Button: React.FC<ButtonProps> = ({ text, onClick, disabled = false }) => {
+  return (
+    <button
+      type="submit"
+      onClick={onClick}
+      disabled={disabled}
+      className={styles.button}
+    >
+      {text}
+    </button>
+  )
+}
+
+export default Button
