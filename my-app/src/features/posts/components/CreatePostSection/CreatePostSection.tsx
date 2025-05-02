cf25fbb9da5bf79db5e772a6abd0565ae060a96f
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-05-01 23:04:29 -0300

    Creates structure for adding post section

---
+++ my-app/src/features/posts/components/CreatePostSection/CreatePostSection.tsx
@@ -0,0 +1,11 @@
+import styles from "./CreatePostSection.module.css"
+
+const createPostSection = () => {
+  return (
+    <div className={styles.createPostSection}>
+      <h2 className={styles.heading}>What's on your mind?</h2>
+    </div>
+  )
+}
+
+export default createPostSection
