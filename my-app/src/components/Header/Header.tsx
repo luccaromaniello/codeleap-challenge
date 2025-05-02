a6dc6e8938c21e529c44199009cc33ea0b10330f
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-05-02 14:22:01 -0300

    Adds post actions structure

--- my-app/src/components/Header/Header.tsx
+++ my-app/src/components/Header/Header.tsx
@@ -1,4 +1,6 @@
 import styles from "./Header.module.css"
+import deleteIcon from "../../assets/icons/delete.svg"
+import editIcon from "../../assets/icons/edit.svg"
 
 type HeaderProps = {
   title: string
@@ -16,6 +18,22 @@ const Header: React.FC<HeaderProps> = ({ title, type = 0 }) => {
       className={`${styles.header} ${type == HeaderType.SECTION ? styles.postHeader : styles.pageHeader}`}
     >
       {title}
+      {type == HeaderType.SECTION ? (
+        <div className={styles.actions}>
+          <img
+            className={styles.actionIcons}
+            src={deleteIcon}
+            alt="Delete action"
+          />
+          <img
+            className={styles.actionIcons}
+            src={editIcon}
+            alt="Edit action"
+          />
+        </div>
+      ) : (
+        ""
+      )}
     </div>
   )
 }
