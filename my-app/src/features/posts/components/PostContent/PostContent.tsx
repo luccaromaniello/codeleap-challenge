f73a4db88abc0e8f1f719197abb801a199988cd3
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-05-02 13:58:03 -0300

    Adds post content behavior

---
+++ my-app/src/features/posts/components/PostContent/PostContent.tsx
@@ -0,0 +1,26 @@
+import styles from "./PostContent.module.css"
+import { timeAgo } from "../../../../utils/math"
+
+type PostContentProps = {
+  content: string
+  author?: string
+  date?: Date
+}
+
+const PostContent: React.FC<PostContentProps> = ({
+  content,
+  author = "Lucca",
+  date = Date.now(),
+}) => {
+  return (
+    <div className={styles.postContent}>
+      <div className={styles.contentHeader}>
+        <span className={styles.author}>@{author}</span>
+        <span className={styles.date}>{timeAgo(Date.now())}</span>
+      </div>
+      <p className={styles.bodyContent}> {content}</p>
+    </div>
+  )
+}
+
+export default PostContent
