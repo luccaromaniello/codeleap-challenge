a5ce580c9514ba820ef32a559e8335499f0d6e31
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-05-03 19:04:46 -0300

    Adds sign out button

--- my-app/src/components/Button/Button.tsx
+++ my-app/src/components/Button/Button.tsx
@@ -1,24 +1,40 @@
 import styles from "./Button.module.css"
 
 type ButtonProps = {
+  type?: ButtonType
   text: string
   onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void
   disabled?: boolean
   loading?: boolean
 }
 
+enum ButtonType {
+  PRIMARY = 0,
+  SECONDARY = 1,
+}
+
 const Button: React.FC<ButtonProps> = ({
+  type = ButtonType.PRIMARY,
   text,
   onClick,
   disabled = false,
   loading = false,
 }) => {
-  return (
+  return type == ButtonType.PRIMARY ? (
     <button
       type="submit"
       onClick={onClick}
       disabled={disabled || loading}
       className={`${styles.button} ${disabled || loading ? styles.disabled : ""}`}
+    >
+      {loading ? "Creating" : text}
+    </button>
+  ) : (
+    <button
+      type="submit"
+      onClick={onClick}
+      disabled={disabled || loading}
+      className={`${styles.button} ${styles.text} ${disabled || loading ? styles.disabled : ""}`}
     >
       {loading ? "Creating" : text}
     </button>
